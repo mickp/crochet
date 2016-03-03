81b0edb87d5ddf120d89431af99911500a93d1db
Mick Phillips <mick.phillips@gmail.com>
2016-03-03 00:25:32 +0000

    A more useful abstraction.

--- crochet.py
+++ crochet.py
@@ -1,12 +1,103 @@
 from math import sin, cos
 
+"""Stitch types:
+    Class   English            US                 Length
+    SC      double             single             1
+    EXSC    extended double    extended single    2
+    HDC     half treble        half double        2
+    DC      treble             double             3
+    TR      double treble      treble             4
+    DTR     triple treble      double treble      5
+"""
+
+
+class Node(object):
+    """A node where stitches meet."""
+    def __init__(self, stitch, prevStitch):
+        # A list of stitches this node forms the head of.
+        self.headOf= []
+        self.nextNode = None
+        if stitch:
+            self.headOf.append(stitch)
+        # A list of stitches this node forms the root of.
+        self.rootOf = []
+        # The node preceding this node.
+        if prevStitch is None:
+            # This is the first stitch.
+            self.prevNode = None
+        else:
+            self.prevNode = prevStitch.head
+            self.prevNode.nextNode = self
+
+        
 class Stitch(object):
+    """A stitch."""
+    # The stitch length.
+    length = None
     # A stitch connecting one root to one head.
-    def __init__(self, root, length):
-        # Another head to act as this stitch's root.
-        self.root = root
-        # The stitch length.
-        self.length = length
+    def __init__(self, into, prev, tog=False, dTheta=0):
+        # A node that acts as a stitch's root.
+        if isinstance(into, Stitch):
+            self.root = into.head
+        elif isinstance(into, Node):
+            self.root = into
+        elif into is None:
+            self.root = None
+        else:
+            raise Exception("2nd argument must be instance of Node or Stitch.")
+        if self.root:
+            self.root.rootOf.append(self)
+        # Previous stitch
+        self.prev = prev
+        if tog:
+            self.head = prev.head
+        else:
+            self.head = Node(self, prev)
+
+
+class ChainStitch(Stitch):
+    length = 1
+    abbrev = 'CS'
+    """A chain stitch."""
+    length = 1
+    def __init__(self, prev=None, dTheta=0):
+        # The previous stitch and root are the same for a chain stitch.
+        super(ChainStitch, self).__init__(prev, prev, tog=False, dTheta=dTheta)
+
+
+class SlipStitch(Stitch):
+    length = 0
+    abbrev = 'SS'
+
+
+class SCStitch(Stitch):
+    length = 1
+    abbrev = 'SC'
+
+
+class EXSCStitch(Stitch):
+    length = 2
+    abbrev = 'EXSC'
+
+
+class HDCStitch(Stitch):
+    length = 2
+    abbrev = 'HDC'
+
+
+class DCStitch(Stitch):
+    length = 3
+    abbrev = 'DC'
+
+
+class TRStitch(Stitch):
+    length = 4
+    ABBREV = 'TR'
+
+
+class DTRStitch(Stitch):
+    length = 5
+    abbrev = 'DTR'
 
 
 class Head(object):
@@ -18,9 +109,9 @@ class Head(object):
         self.length = 1
         # Stitches into this head.
         self.stitches = []
-        # The width of the stitch.
+        # The width of the head.
         self.width = width
-        # The orientation of the stitch.
+        # The orientation of the head.
         self.theta = None
         # The position of the head.
         self.pos = (None, None)
@@ -37,30 +128,50 @@ class Head(object):
             self.pos = (x, y)
 
 
-class Chain(Head):
-    # A chain stitch.
-    def __init__(self, prev=None, dTheta=0):
-        super(Chain, self).__init__(prev, width=1, dTheta=dTheta)
-
-
 class Pattern(object):
     def __init__(self):
-        self.start = Chain()
-        self.prev = self.start
-
-
-    def crochet(self, into, tog=False, dTheta=0):
-        if tog:
-            head = self.prev
-        else:
-            head = Head(self.prev, width=1, dTheta=dTheta)
-        head.stitches.append(Stitch(into, length=1))
-        self.prev = head
+        self.start = ChainStitch()
+        self.lastStitch = self.start
+        self.lastRoot = None
 
 
     def chain(self, dTheta=0):
-        head = Chain(self.prev, dTheta=dTheta)
-        self.prev = head
+        self.lastStitch = ChainStitch(self.lastStitch, dTheta)
+        self.lastRoot = self.lastStitch.prev.head
 
 
+    def workInto(self, stitchType, nodeOrStitch, **kwargs):
+        if isinstance(nodeOrStitch, Stitch):
+            self.lastRoot = nodeOrStitch.head
+        elif isinstance(nodeOrStitch, Node):
+            self.lastRoot = nodeOrStitch
+        else:
+            raise Exception('2nd argument must be a node or stitch.')
+        self.lastStitch = stitchType(self.lastRoot, self.lastStitch, **kwargs)
 
+
+    def workIntoNext(self, stitchType, **kwargs):
+        self.workInto(stitchType, self.lastRoot.nextNode)
+    
+
+    def workIntoSame(self, stitchType, **kwargs):
+        self.workInto(stitchType, self.lastRoot)
+
+
+    def getAllNodes(self):
+        nodes = set()
+        stitch = self.lastStitch
+        while stitch:
+            nodes.add(stitch.head)
+            stitch = stitch.prev
+        return nodes
+
+
+    def forwardIter(self):
+        node = self.start.head
+        while node:
+            for stitch in node.headOf:
+                yield stitch
+            node = node.nextNode
+
+        
